4586ff4e38ac4b6079acb14324aae457550d7dc2
KissDuo <303135526@qq.com>
2018-11-22 20:02:39 +0800

    getIp

--- public/js/util.js
+++ public/js/util.js
@@ -12,6 +12,17 @@ Util.getDate = function getDate(){
 
     var str = year+'年'+month+'月'+day+'日 '+hour+':'+minute+':'+second;
     return str;
-}
+};
+
+//通过req的hearers来获取客户端ip
+Util.getIp = function(req) {
+    var ip = req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress || '';
+    if(ip.split(',').length>0){
+        ip = ip.split(',')[0];
+    }
+    return ip;
+};
 
 module.exports = Util;
