713f4a168d2ba12dad444e9669d9c4053047747c
KissDuo <303135526@qq.com>
2018-12-01 14:56:30 +0800

    增加获取列表接口

--- routes/question.js
+++ routes/question.js
@@ -6,6 +6,46 @@ var dbConfig = require("../database/DBConfig");
 var pool = mysql.createPool(dbConfig.mysql );
 
 /*获取问题列表*/
+router.get('/getList', function(req, res, next) {
+    pool.getConnection(function(err,connection) {
+        var sql_get_question = "select * from question";
+        connection.query(sql_get_question, function (err, result) {
+            try{
+                if(result && result.length > 0){
+                    var data = [];
+                    for(var i=0;i<result.length;i++){
+                        result[i].result = eval(result[i].result);
+                        result[i].pages = eval(result[i].pages);
+                        data.push(result[i]);
+                    }
+                    var res_json = {
+                        status_code : 1,
+                        msg : "查询成功",
+                        result : data
+                    };
+                    console.log("查询问题列表成功");
+                    res.json(res_json);
+                }else{
+                    console.log("查询问题失败");
+                    res.json({
+                        status_code : "1",
+                        msg:"找不到问题"
+                    });
+                }
+            }catch (e) {
+                console.log(e);
+                res.json({
+                    status_code:"-200",
+                    msg:"查询失败"
+                });
+            }
+            //释放链接
+            connection.release();
+        });
+    });
+});
+
+/*获取问题详情*/
 router.get('/getInfo', function(req, res, next) {
     var qid = req.query.qid || "";
     pool.getConnection(function(err,connection) {
