ad634857e41e1853c495fe0a2e50b7ddb0fbc084
KissDuo <303135526@qq.com>
2018-12-01 10:50:48 +0800

    删掉关闭db操作

--- routes/question.js
+++ routes/question.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 var Util = require('../public/js/util.js');
-
-var url = "mongodb://39.105.217.30:27017/";
+var mysql = require("mysql");
+var dbConfig = require("../database/DBConfig");
+var pool = mysql.createPool(dbConfig.mysql );
 
 /*获取问题列表*/
 router.get('/getInfo', function(req, res, next) {
-
+    var qid = req.query.qid || "";
+    pool.getConnection(function(err,connection) {
+        var sql_get_question = "select * from question where qid = '" + qid + "'";
+        connection.query(sql_get_question, function (err, result) {
+            try{
+                if(result && result.length > 0){
+                    var res_json = {
+                        status_code : 1,
+                        msg : "查询成功",
+                        data : result[0]
+                    };
+                    console.log("查询问题qid:"+qid+"成功");
+                    res.json(res_json);
+                }else{
+                    console.log("查询问题qid:"+qid+"不存在");
+                    res.json({
+                        status_code : "1",
+                        msg:"问题不存在"
+                    });
+                }
+            }catch (e) {
+                console.log(e);
+                res.json({
+                    status_code:"-200",
+                    msg:"查询失败"
+                });
+            }
+            //释放链接
+            connection.release();
+        });
+    });
 });
 
 
